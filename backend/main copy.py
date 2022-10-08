141d0cc1e7410d2987602e2c89bf2bdeee549d60
Mahri <mahri.almaz@gmail.com>
2022-10-08 19:12:19 -0400

    edit collection variables

--- backend/main copy.py
+++ backend/main copy.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson import json_util
 
 
-DB_NAME = 'InsideTheGradebook'
+DB_NAME = 'Gradebook'
 COLLECTION_STUDENTS = 'Students'
 COLLECTION_GRADES = 'Grades'
 COLLECTION_TEACHERS = 'Teachers'
@@ -17,7 +17,13 @@ client = MongoClient("mongodb://127.0.0.1:27017/:27017")
 
 db = client[DB_NAME]
 
-coll1 = db[COLLECTION_NAME] #selecting the coll1 in myDatabase
+coll_s = db[COLLECTION_STUDENTS] #selecting the students in the database
+coll_g = db[COLLECTION_GRADES] #selecting the grades in the database
+coll_t = db[COLLECTION_TEACHERS] #selecting the teachers in the database
+coll_c = db[COLLECTION_CLASSES] #selecting the classes in the database
+coll_a = db[COLLECTION_ASSIGNMENTS] #selecting the assignments in the database
+
+
 # for document in coll1.find():
 #     print (document)
 
