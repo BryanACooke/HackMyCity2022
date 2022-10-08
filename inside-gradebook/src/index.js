070934b992f210430583913d530729f321d33bce
BryanACooke <bryancooke02@gmail.com>
2022-10-08 10:25:47 -0400

    Created Nav Bar with Other Components

--- inside-gradebook/src/index.js
+++ inside-gradebook/src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './jspages/header'
+import App from './App.js';
+import StudentPage from './jspages/studentPage.js'
+import MainDashboard from './jspages/mainDashboard.js'
+
+ReactDOM.render(
+  <Router>
+  <Header />
+ <Routes>
+  <Route exact path="/" element={<App/>} />
+ <Route path="/mainDashboard" element={<MainDashboard/>} /> 
+ <Route path="/studentPage" element={<StudentPage/>} />
+
+  </Routes>
+  
+
+</Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ inside-gradebook/src/jspages/header.js
@@ -0,0 +1,48 @@
+import {React, useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
+import {Navbar, Nav, Container, Form, Button, FormControl } from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+import '../csspages/header.css'
+import {useNavigate} from "react-router-dom"
+
+
+
+function Header(){
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    /* A hook that is used to get the current user and if the user is authenticated. */
+    let navigate = useNavigate();
+  
+    return (
+  <div className = "theBar">
+  {/* The code for the header of the website. It is a navbar that has a link to the search page, a link to the top
+  movies page, a link to the watchlist page, and a link to the user profile page. */}
+  <Navbar  className= "testClass "  bg="" variant="dark">
+    <Container id="containerMain">
+    <Navbar.Brand className="fs-2"><Link to ="/">InsideTheGradebook</Link></Navbar.Brand>
+    <Nav className="mx-right navholder">
+    <Form className="d-flex align-items-center ">
+    <Nav.Link  className="fs-4 elementTabTwo"><Link to ="/mainDashboard">Dashboard</Link></Nav.Link> 
+        </Form>
+  <h1 class="navBreaker">|</h1>
+       <Navbar.Text id="textSignedInAs"><div id="bothTexts"><div id="firstText">Signed in as: </div><div id="break">
+       <Link to="/UserProfile">John Doe</Link>
+       </div> </div><Link to="/UserProfile"><div id="profilePicture">
+       <img id="imgProfilePicture" src="https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png"
+          width="55"
+          height="55"
+          alt="Profile Photo"
+          
+        />
+       </div>
+       </Link>
+       </Navbar.Text>
+       
+   </Nav>
+    </Container>
+  </Navbar>
+  </div>
+      )
+  }
+  
+  export default Header
