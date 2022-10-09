5072e635918fd0eddcbc5030fc2d4d1386b45765
BryanACooke <bryancooke02@gmail.com>
2022-10-09 07:22:09 -0400

    Extra Info 2.0

--- inside-gradebook/src/App.js
+++ inside-gradebook/src/App.js
@@ -18,8 +18,9 @@ function App() {
           <h1 className="mainTitle">InsideTheGradebook</h1>
           </div>
           <div className="contentDetailedText">
-            <p>Inside The Gradebook provides a clean, simple, and easy to view student progress
-              overview. You can specify which classes you  </p>
+            <p>Inside The Gradebook provides a clean, simple, and easy way to view student progress
+              overview. View your student's progress in a new way, allowing for a more impactful and
+              immersive teaching experience.  </p>
           
         </div>
         
